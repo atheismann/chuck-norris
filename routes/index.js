edc16606cd84ebd10785bc823c35f81bd29c3346
Andrew Theismann <andrew@theismannconsulting.com>
2019-08-19 16:48:35 -0500

    complete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const chuckCtrl = require('../controllers/chuck');
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', chuckCtrl.index);
 
 module.exports = router;
